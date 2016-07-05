29dcec3006eeca2416fabbd7ce599fc36eb7f8f2
Dan Secrest <dsecrest@hugeinc.com>
2016-07-05 14:18:49 -0400

    Remove obsolete browser versions from autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('styles', function() {
             errLogToConsole: true
         }))
         .pipe(plugins.autoprefixer({
-            browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+            browsers: ['last 2 version']
         }))
         .pipe(development(plugins.sourcemaps.write()))
         .pipe(production(plugins.minifyCss()))
